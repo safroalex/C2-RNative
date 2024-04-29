6afa42b06675b21c2e15b219b81ae1f1ec1ef479
istaeverd3@gmail.com <istaeverd3@gmail.com>
2024-04-29 21:13:25 +0300

    add help

--- korennie_narodi/src/components/Help/Help.js
+++ korennie_narodi/src/components/Help/Help.js
@@ -1,12 +1,73 @@
 import React from 'react';
+import Header from '../Header/header';
+import Footer from '../Footer/footer';
+import line from './../../images/line.jpg';
+import './Help.css';
+import lenobl from "../../images/len-obl.png";
+import pribaikal from "../../images/pribaikal.png";
+import sever from "../../images/sever.png";
+import forest from "../../images/Rectangle 4934.png"
+import dalnVostok from "../../images/daln-vostok.png"
+import mamont from "../../images/eff-mamonta.png"
+import arctika from "../../images/dobrov-arktiki.png"
+
 
 function Help() {
-  return (
-    <div>
-      <h1>Помощь</h1>
-      <p>Это страница, на которой обсуждаются способы помощи.</p>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <div className="help">
+                <div className="title-help">
+                    <p>помощь коренным народам</p>
+                </div>
+                <div className='line-div'>
+                    <img src={line} alt="" />
+                    <img src={line} alt="" />
+                </div>
+
+                <div className='help-sec1'>
+                    <div className='help-box border-none'>
+                        <img className='image-help' src={lenobl} alt="" />
+                        <p className='text-help'> Центр коренных народов Ленинградской области  культурно-просветительский центр, созданный с целью сохранения и развития культуры коренных народов Ленинградской области (ижоры, води, ингерманландских финнов, вепсов и тихвинских карел).</p>
+                    </div>
+                    <div className='help-box border-none'>
+                        <img className='image-help' src={pribaikal} alt="" />
+                        <p className='text-help'>Центр культуры коренных народов Прибайкалья  центр, созданный с целью возрождения, сохранения и развития традиционной культуры коренных народов Прибайкалья, приобщение к ней жителей, туристов и гостей Иркутской области.</p>
+                    </div>
+                    <div className='help-box border-none border-right-none'>
+                        <img className='image-help' src={sever} alt="" />
+                        <p className='text-help'>Коренные малочисленные народы Севера      группа посвящена культурным мероприятиям и событиям народов Севера. </p>
+                    </div>
+                </div>
+                <div className="help-sec2">
+                    <div className="conatiner1">
+                        <p className="text-forum">Форум молодежи коренных малочисленных народов Севера, Сибири и Дальнего Востока «Российский Север»
+                            <br/><br/>Форум проводится с целью создания межнациональной коммуникационной площадки для взаимодействия молодежи и вовлечения активных молодых людей в решение задач, отвечающих современным запросам государства и общества.</p>
+                        <img className="image-forest" src={forest} alt=""/>
+
+                    </div>
+                </div>
+                <div className="help-sec3">
+                    <div className="help-box-2">
+                        <img src={dalnVostok} alt="" className="image-help-2"/>
+                        <p className='text-help text-width'>История коренных народов Дальнего Востока
+                            <br/><br/>Группа посвящена истории, религия, быту, культуре коренных народов Дальнего Востока.</p>
+                    </div>
+                    <div className="help-box-2">
+                        <img src={mamont} alt="" className="image-help-2"/>
+                        <p className='text-help text-width'>Фонд сохранения и поддержки природного и культурного наследия «Эффект Мамонта»
+                            <br/><br/>Фонд объединяет ученых, исследователей, путешественников и представителей творческой сферы для того, чтобы трудиться над сохранением уникального наследия нашей планеты.</p>
+                    </div>
+                    <div className="help-box-2">
+                        <img src={arctika} alt="" className="image-help-2"/>
+                        <p className='text-help text-width'>Фонд сохранения и поддержки природного и культурного наследия «Эффект Мамонта»
+                            <br/><br/>Фонд объединяет ученых, исследователей, путешественников и представителей творческой сферы для того, чтобы трудиться над сохранением уникального наследия нашей планеты.</p>
+                    </div>
+                </div>
+            </div>
+            <Footer/>
+        </>
+    );
 }
 
 export default Help;
